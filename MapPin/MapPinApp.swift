2edd5557e3bbba855fdda646f84ed9e4e0de41ed
Aziza <aziza.elgoul1@gmail.com>
2024-05-30 15:49:15 +0200

    Initial Commit

---
+++ MapPin/MapPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPinApp.swift
+//  MapPin
+//
+//  Created by KARMANI Aziza on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
